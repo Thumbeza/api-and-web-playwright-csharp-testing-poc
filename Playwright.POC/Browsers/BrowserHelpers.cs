c8974c0545e7ec2736764c57ca8493a7d02b7b6b
Siphesihle Hlophe <siphesihle.hlophe@structureit.net>
2022-10-10 13:06:48 +0200

    adding browser helpers

---
+++ Playwright.POC/Browsers/BrowserHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace Playwright.POC.Browsers
+{
+    public class BrowserHelpers
+    {
+        private readonly IPage _page;
+
+        public BrowserHelpers(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task<string> TakeScreenshot()
+        {
+            var bytes = await _page.ScreenshotAsync();
+
+            return Convert.ToBase64String(bytes);
+        }
+        
+        public async Task TakeScreenshot(string filePath)
+        {
+            await _page.ScreenshotAsync(new PageScreenshotOptions
+            {
+                Path = filePath,
+                FullPage = true,
+            });
+        }
+    }
+}
